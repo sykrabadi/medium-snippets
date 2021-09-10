efad165587a80f67f1925cb95a40de717ceb5c18
sykrabadi <muhammad.syukur.1905356@students.um.ac.id>
2021-09-10 21:16:16 +0700

    Create function1.py

---
+++ Revisiting Recursion/function1.py
@@ -0,0 +1,19 @@
+def retrunVal(val1, val2):
+    result = val1 + val2
+    return result
+
+def notReturnVal(val1, val2):
+    result = val1 + val2
+    return
+
+a = 2
+b = 3
+
+c = retrunVal(a, b)
+d = notReturnVal(a, b)
+
+print(c)
+print(d)
+
+c = c + 2
+print(c)
